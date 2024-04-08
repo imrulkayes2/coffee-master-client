986c050f2e9dbaab9ea099f0f8764e7ccc338eb1
imrulkayes75 <ikbubt@gmail.com>
2024-04-08 19:10:56 +0600

    coffee master client site created

---
+++ src/Components/UpdateCoffee/UpdateCoffee.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UpdateCoffee = () => {
+    return (
+        <div>
+            <h3>Update Coffee information here</h3>
+        </div>
+    );
+};
+
+export default UpdateCoffee;
